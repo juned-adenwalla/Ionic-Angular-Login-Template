e6bc73850eda5fc9042b92ccfd125e8268d0f998
Juned Adenwalla <adenwalla.juned@gmail.com>
2022-09-11 14:12:38 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'juned',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
